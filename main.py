f0a53a2aa4e57bbdd865399d871c4227aece6397
halas-j <halas-j@proton.me>
2024-11-08 12:41:51 -0800

    - added the CIFAR-100 dataset - built a neural network to classify a dataset - implemented optimisation of model's parameters - added time measurements to the benchmark

--- main.py
+++ main.py
@@ -1,6 +1,115 @@
 import torch
+from torch import nn
+from torch.cuda import device
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import time
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(3 * 32 * 32, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 100),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # Set the model to training mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.to("cuda")
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X.cuda())
+        loss = loss_fn(pred, y.cuda())
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X.cuda())
+            test_loss += loss_fn(pred, y.cuda()).item()
+            correct += (pred.argmax(1) == y.cuda()).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 if __name__ == '__main__':
 
-    x = torch.cuda.is_available()
-    print(x)
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    training_data = datasets.CIFAR100(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
+
+    test_data = datasets.CIFAR100(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor()
+    )
+
+    batch_size = 400
+
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, pin_memory=True, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size*2, pin_memory=True, shuffle=True)
+
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    learning_rate = 1e-3
+
+    epochs = 100
+
+    start_time = time.time()
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+    print("Done! It took %s seconds to train" % (time.time()-start_time))
